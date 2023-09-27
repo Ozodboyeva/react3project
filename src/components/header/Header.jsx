9d30fc28e3ededff7018313a74aec54c957ad2ee
MuharramOzodboyeva <muharramozodboyeva598@gmail.com>
2023-09-27 15:17:10 +0500

    first commit

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -6,7 +6,7 @@ export class Header extends Component {
     return (
       <InputGroup className="my-3">
         <Form.Control placeholder="Searching todo" />
-        <InputGroup.Text id="basic-addon2">@example.com</InputGroup.Text>
+        <InputGroup.Text id="basic-addon2">Contact sections</InputGroup.Text>
       </InputGroup>
     );
   }
